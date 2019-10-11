e4dfd91e21d68b2e07117cbfe0efba816b3360dd
Alexander <shvetcov2000@gmail.com>
2019-10-11 15:24:30 +0300

    add input validation, win option and moves loop until game over

--- bin/main.rb
+++ bin/main.rb
@@ -7,39 +7,41 @@ class Game
     @player2 = ''
     @board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     # Greetings start
-    puts 'Hello! Welcome, Lets PLAY TIC TAC TOE ' # TODO: Need beautiful greeting
+    puts 'Hello! Welcome, Lets PLAY TIC TAC TOE '
 
     # First player name (repeat until if empty):
     while @player1.empty?
-      puts 'What is your name Player One'  # TODO: Ask name of first player
+      puts 'What is your name Player One'
       @player1 = gets.chomp
-      puts 'Please repeat and insert your name Player One' if @player1.empty? # TODO: Report about empty inpit and ask again
+      puts 'Please repeat and insert your name Player One' if @player1.empty?
       next
     end
 
     # Second player name (repeat until if empty):
     while @player2.empty?
-      puts 'What is your name Player Two' # TODO: Ask name of second player
+      puts 'What is your name Player Two'
       @player2 = gets.chomp
-      puts 'Please repeat and insert your name Player Two' if @player2.empty? # TODO: Report about empty inpit  and ask again
+      puts 'Please repeat and insert your name Player Two' if @player2.empty?
       next
     end
 
     # Greetings wrap
-    puts "Oki-doki, lets play  #{@player1} and #{@player2}. 
-    Let the best win Ready, Steady GOO..." # TODO: Wrap up greeting and announce game start
+    puts "Oki-doki, lets play  #{@player1} and #{@player2}.
+    Let the best win Ready, Steady GOO..."
 
     # Display current board
     display
 
-    # Ask for move first player
-    move(@player1)
+    # Loop moves until over
+    until over?
+      # Ask for move first player
+      move(@player1)
 
-    # Ask for move second player
-    move(@player2)
+      # Ask for move second player
+      move(@player2)
+    end
 
-    # Declare result
-    result('Draw')
+    result
   end
 
   def display
@@ -54,26 +56,39 @@ class Game
 
   def move(player)
     @square = nil
-    while @square.nil? || @square > 9 || @square < 1
-      puts "Tell me you move, #{player} pick a square" # TODO: Ask for move
-      begin
-        @square = gets.chomp
-        @square = Integer(@square)
-      rescue ArgumentError
-        puts "Pick Integer from 1 to 9 .Tell me you move, #{player} pick a square!"  # TODO: Ask again if input invalid 
-        retry
-      end
+    until validate(@square)
+      puts "Tell me your move, #{player} pick a square"
+      @square = gets.chomp.to_i
     end
+    display
   end
 
-  def result(res)
-    puts "It is a #{res} guys, well played. One more? (Yes / No)" # TODO: Declare result and ask for new game
+  def validate(square)
+    return false if square.nil?
+
+    unless square.between?(1, 9)
+      puts 'Pick Integer from 1 to 9 '
+      return false
+    end
+
+    true
+  end
+
+  def over?
+    [true, false].sample
+  end
+
+  def winner
+    ['Player One', 'Player Two', false].sample
+  end
+
+  def result
+    if winner
+      puts "Winner is #{winner}. One more? (Yes / No)"
+    else
+      puts 'It is a Draw guys, well played. One more? (Yes / No)'
+    end
     Game.new if gets.chomp == 'Yes'
   end
 end
 Game.new
-
-
-
-
-

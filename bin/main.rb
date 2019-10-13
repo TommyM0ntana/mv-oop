a5a031debd0e61856037fb8cf926757017f0521a
Alexander <shvetcov2000@gmail.com>
2019-10-13 16:01:22 +0300

    clean up comments

--- bin/main.rb
+++ bin/main.rb
@@ -1,1 +1,74 @@
+# frozen_string_literal: true
 
+# Game
+class Game
+  require_relative '../lib/player'
+  require_relative '../lib/board'
+  def initialize
+    @player1 = nil
+    @player2 = nil
+    @board = nil
+
+    puts 'Hello! Welcome, Lets PLAY TIC TAC TOE '
+
+    while @player1.nil? || @player1.empty?
+      print 'What is your name Player One: '
+      @player1 = gets.chomp
+      print 'Please repeat and insert your name Player One: ' if @player1.empty?
+      next
+    end
+    @player1 = Player.new(@player1, 'X')
+
+    while @player2.nil? || @player2.empty?
+      print 'What is your name Player Two: '
+      @player2 = gets.chomp
+      print 'Please repeat and insert your name Player Two: ' if @player2.empty?
+      next
+    end
+    @player2 = Player.new(@player2, 'O')
+
+    @board = Board.new
+
+    display
+
+    @board.turn == 'X' ? move(@player1.name, @player1.sign) : move(@player2.name, @player2.sign) until @board.over
+
+    result(@board.winner)
+  end
+
+  def valid(square)
+    return false if square.nil?
+
+    until square.between?(1, 9)
+      puts 'Please pick Integer from 1 to 9 '
+      return false
+    end
+    true
+  end
+
+  def move(_name, sign, square = nil)
+    square = gets.chomp.to_i until valid(square)
+    @board.move(sign, square)
+    display
+  end
+
+  def result(winner)
+    if winner
+      name = winner == 'X' ? @player1.name : @player2.name
+    else
+      text = 'It is a Draw guys, well played!'
+    end
+    puts text
+    puts 'One more? (Yes / No)'
+    Game.new if gets.chomp == 'Yes'
+  end
+
+  def display
+    squares = @board.squares
+    puts ' '
+    puts ' --+---+---'
+    puts ' --+---+---'
+    puts ' '
+  end
+end
+Game.new
--- lib/board.rb
+++ lib/board.rb
@@ -1,39 +1,25 @@
 # frozen_string_literal: true
 
-# Store board information
 class Board
   attr_reader :squares, :wins, :turn, :over, :winner
   def initialize
-    # Available squares
     @squares = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    # Win combinations
     @wins = [[1, 4, 7], [2, 5, 8], [3, 6, 9], [3, 5, 7], [1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 5, 9]]
-    # Players moves history
     @moves = { 'X' => [], 'O' => [] }
-    # Who's move
     @turn = 'X'
-    # Game over?
     @over = false
-    # Is there the winner?
     @winner = nil
   end
 
-  # Name winner if there is one
   def find_winner(sign)
     @winner = sign if @wins.any? { |w| w.all? { |e| @moves[sign].include?(e) } }
   end
 
-  # Process move
   def move(sign, square)
-    # Mark square
     @squares = @squares.each_with_index.map { |e, i| i == square - 1 ? sign : e }
-    # Swap move turn
     @turn = @turn == 'X' ? 'O' : 'X'
-    # Update moves history
     @moves[sign] << square
-    # Check if there is a winner
     find_winner(sign)
-    # Check if game over
     @over = true if !@winner.nil? || (@moves['X'] + @moves['O']).length == 9
   end
 end
